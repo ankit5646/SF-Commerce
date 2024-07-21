f733a6c2fdb7fdbb2853d7f46fe91d90e5add78a
Ankit yadav <118676491+ankit5646@users.noreply.github.com>
2024-07-21 12:20:45 +0000

    finalizing

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,15 @@ module.exports = {
           '50%': { borderRadius: '30% 60% 70% 40% / 50% 60% 30% 60%' },
           '100%': { borderRadius: '60% 40% 30% 70% / 60% 30% 70% 40%' },
         },
+
+        "slider" : {
+          form:{transform: "translateX(0)"},
+          to:{transform: "translateX(-100%)"}
+        },
       },
       animation: {
         morph: 'morph 8s ease-in-out infinite',
+        "slider": "slider 20s linear infinite",
       },
       borderRadius: {
         'custom-1': '60% 40% 30% 70% / 60% 30% 70% 40%',
